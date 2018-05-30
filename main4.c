b5eff7565f3cb06f4942b6f2be97786a8f44704e
DChsu8856 <jerry8856hsu@gmail.com>
2018-05-30 22:27:40 +0800

    指向常數資料的常數指標

---
+++ main4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	 const int const number[10] = {9,2,3,1,5,4,8,7,10,6};
+	int i = 0, j = 0;
+    int temp = 0;
+   bs(number,number);
+
+    for( i = 0; i < 10; i++ ) {
+        printf("%d ", number[i]);
+    }
+
+}
+
