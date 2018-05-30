9a7c8988c0b8a31b75a81d6a15f888cb2b43fd26
DChsu8856 <jerry8856hsu@gmail.com>
2018-05-30 22:26:10 +0800

    指向非常數資料的常數指標

---
+++ Untitled2.c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+void bs(int *const number){
+    int i = 0, j = 0;
+    int temp = 0;
+
+    for( i = 0; i < 10; i++) {
+        for( j = i; j < 10; j++) {
+            if( number[j] > number[i] ) {
+                temp = number[j];
+                number[j] = number[i];
+                number[i] = temp;
+            }
+        }
+    }
+}
